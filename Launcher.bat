f660f1e31bc783f078ed1f1815a42cf4a5b947e4
MrT <tom.harrison@hotmail.ch>
2020-03-01 05:00:09 +0100

    First Draft
    Currently only Roughly tested, tho it should do its job =)

---
+++ Launcher.bat
@@ -0,0 +1,148 @@
+@Echo OFF
+REM Author MrT, Version 0.2
+:: Just a "Wrapper" around MWC Node and Wallet to make interaction a bit more Userfriendly
+:: Replace the Variables if Needed I assume the following: 
+:: This File, MWC-Wallet.exe and MWC.exe are all in the same Folder!
+:: Hint: You will need to type your password for some interactions, please stay safe out there!
+REM Never share customized scripts that might contain passwords! (Hence this Script doesnt ask for a password or gives a way to include it)
+
+REM TO EDIT!  Fixxed Variables (Replace if necessary!)
+REM Define Folders of our executables (by default our execution directory)
+	set NodeLocation=%cd%
+	set WalletLocation=%cd%
+	set TransactionFilename=transaction.tx
+	set Responsefilename=tx.response
+	
+REM End of Editable part, doing some simple Logic down there 	
+
+REM Setup
+	REM Make sure everything is as we assume 
+	REM Sanity Check time <3
+		IF EXIST "%NodeLocation%\mwc.exe" (
+			Echo [INFO:] Located Node
+		) ELSE (
+			Echo [ERROR:] Cannot locate Node! Please make sure your mwc.exe is actually saved under %NodeLocation% or edit the variable NodeLocation
+			goto Quit
+		)
+		
+		IF EXIST "%WalletLocation%\Backups\" (
+			Echo [INFO:] Located Backup Folder for Slatefiles
+		) ELSE (
+		mkdir %WalletLocation%\Backups\
+			Echo [INFO:] Created Backup Folder for processed Slatefiles in %WalletLocation%\Backups\
+			)
+			
+		IF EXIST "%WalletLocation%\mwc-wallet.exe" (
+			Echo [INFO:] Located Wallet
+		) ELSE (
+			Echo [ERROR:] Cannot locate Wallet! 
+			Echo Please make sure your mwc.exe is actually saved under:
+			Echo %WalletLocation% or edit the variable WalletLocation
+			goto Quit
+		)
+			
+REM Setup Node as needed for everything
+	cd %NodeLocation%\
+	start /min mwc.exe
+	Echo [WARN:] MWC-Node starting please give it a Moment to synchronize!
+	Echo.
+		
+REM Define Interactive modes (Ask for startup vars?)
+	Echo "What do you want to do? (Type letter and press Enter)"
+	Echo.
+	set /p mode=(S)end, (F)inalize, (L)isten, (I)nfo, (C)ommandprompt
+	Echo.
+		GOTO %mode%
+
+REM ####Modes####
+	
+:S
+:s	
+	
+	
+	cd %WalletLocation%\
+	REM Send a transaction, ask which mode 
+	Echo.
+	Echo (Hint: Type "File" or "HTTP" completely!)
+	set /p method=Send by (File) or (HTTP) 
+	set /p Amount=What Amount to send?
+	
+	REM Move old Slatefile in Backupfolder
+	IF EXIST "%WalletLocation%\%TransactionFilename%" (
+		Echo [INFO:]Moved old Slatefile to Backups before creating new one
+		move %WalletLocation%\%TransactionFilename% %WalletLocation%\Backups\%TransactionFilename%
+	) ELSE (
+		REM not needed but here cuz im lazy, find the egg, keep it =) 
+	)
+	
+	goto %method%
+		
+		:File
+		:file
+			mwc-wallet.exe send -m file -d %TransactionFilename% %Amount%
+			Echo [INFO:]Your payment File will be located in %WalletLocation%\%TransactionFilename%
+			Echo.
+				goto Redo
+		:HTTP
+		:http
+			mwc-wallet.exe send -d %URL% %Amount%
+			Echo.
+				goto Redo
+				
+:F
+:f
+	REM Check if we can find the file to process, if not warn user!
+	IF EXIST "%WalletLocation%\%Responsefilename%" (
+		Echo [INFO:] Located a Responsefile matching variable
+	) ELSE (
+		Echo [WARN:] Cannot locate Responsefilename! Might not exist yet
+		Echo Please make sure your Responsefile is actually saved under: 
+		Echo %WalletLocation% and is named %Responsefilename% or edit the variable Responsefilename
+	)
+	
+	REM Finalize a transaction
+	mwc-wallet.exe finalize -i %Responsefilename%
+	Echo.
+		goto Redo
+	
+:L
+:l
+	REM Wallet listen mode
+	start cmd.exe /k "Echo Enter your password to start listening!&&Echo Then return the the Launcher!&& mwc-wallet.exe listen"
+	rem Echo Enter your password in the newly entered windows and your Wallet will be listening!
+	Echo.
+		goto Redo
+	
+:I
+:i
+	mwc-wallet.exe info
+	Echo.
+
+
+:Redo
+	REM Define Interactive modes (Ask for startup vars?)
+	Echo "Anything else - or do you want to Quit?"
+	set /p mode=(S)end, (F)inalize, (L)isten, (I)nfo,(C)ommandprompt, (Q)uit
+		GOTO %mode%
+
+	
+:Q
+:Quit
+	Echo This Window will close itself, the MWC-Wallet and the MWC-Node as soon as you press [Enter]
+	Echo [WARN:] This will close all of your command prompts!
+	Echo Stay safe out there! 
+	pause
+	taskkill /IM mwc.exe /F
+	taskkill /IM mwc-wallet.exe  /F
+	taskkill /IM cmd.exe  /F
+	quit
+REM Interactive Shell/Commandprompt overtakes this current session, give control back to uer 	
+:C
+:c
+cmd /k Echo type mwc-wallet.exe help to get a list of commands!
+rem start cmd.exe /k "cd %WalletLocation%\&&Echo type mwc-wallet.exe help to get a list of commands!"
+
+REM Dev Notes
+REM To Implement:
+REM How to Grab Ngrok HTTP Tunnel address? 
+REM I'd like to Auto-implement ngrok, need to read the docs on how to grab the URL somehow
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # MWC Launcher
  A Script to Wrap around the MWC Wallet and MWC Node to make transactions easier using the CLI Wallet
+ 
+ Simply Copy the Launcher into the same Directory as your mwc-wallet.exe and mwc.exe
+ 
+ This script assumes the following: 
+ This File, MWC-Wallet.exe and MWC.exe are all in the same Folder!
+ 
+ Replace the following Variables if Needed:
+	NodeLocation=PathofyourNodeDirectory
+	WalletLocation=PathofyourCLIWallet
+	TransactionFilename=Whatevernameyourtransactionfilesshouldhave
+	Responsefilename=thenameofyourreceivedresposefile
+
+	
+Remarks: 
+I didn't include the password parameter intentionally. 
+For Security concerns I think this should only be done by advanced users and can easily be implmeneted if you'd want to!
