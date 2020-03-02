895753e413bb4344011cc14dabd573a842674adc
MrT <tom.harrison@hotmail.ch>
2020-03-02 03:16:32 +0100

    Everything minimized now
    Fully wrapped  (I will probably never include init or restore, only support it with the Command prompt mode in this script)

--- Launcher.bat
+++ Launcher.bat
@@ -1,58 +1,48 @@
 @Echo OFF
-REM IMPORTANT: DONT SHARE YOUR BATCHFILES
-REM TAKE SECURITY SERIOUS! STAY SAFE
+REM IMPORTANT: DONT SHARE YOUR BATCHFILES - TAKE SECURITY SERIOUS! STAY SAFE
 	
-REM TO EDIT!  
+REM Edit your password so you don't constantly need to confirm the password when interacting with the cli Wallet!  
 	set mypassword=NEVERSHAREYOURBATCHFILESORPASSWORD!
 
-
-
-REM Only replace if necessary
+	
+REM Only replace the following Variables if necessary (not everything in 1 folder)
 
 REM Define Folders of our executables (by default our execution directory)
 REM Some examples provided below, if in Doubdt, Rightclick the folder in Windows Explorer and choose "Copy Path"
-
-REM An Example if the Wallet is in a subfolder called "mwc-wallet" => 
-REM set WalletLocation=%cd%\mwc-wallet
-
-REM An Example if the Wallet AND this Script Are in a subfolder and the Node is "above" us => 
-REM set NodeLocation=%cd%\..\
-
 	set NodeLocation=%cd%
 	set WalletLocation=%cd%
-REM Ngrok is optional but heavilly recommended!
 	set NgrokLocation=%cd%
-REM Names of Slate Files 
 	set TransactionFilename=transaction.tx 
 	set Responsefilename=tx.response
-REM Set to "TRUE" for detailed messaged, to "FALSE" if not
+	REM Set to "TRUE" for detailed messaged, to "FALSE" if not
 	set Debugmode=FALSE
-REM Set to "TRUE" if Launcher should Quit instantly when choosing "quit"
+	REM Set to "TRUE" if Launcher should Quit instantly when choosing "quit"
 	set CloseFast=FALSE
-	
-REM End of Editable part, doing some simple Logic down there 	
+	REM An Example if the Wallet is in a subfolder called "mwc-wallet" => 
+		REM set WalletLocation=%cd%\mwc-wallet =>
+	REM An Example if the Wallet AND this Script Are in a subfolder and the Node is "above" us => 
+		REM set NodeLocation=%cd%\..\
+	REM An Example for a copied path => 
+		REM set NodeLocation=C:\_Custom\Bitcoin_Wallets\mwc\CLI\mwc-wallet
 
+REM No Further editing needed, Logic part down here
 
-REM Setup
-	REM Make sure everything is as we assume 
-	REM Sanity Check time <3
-		
+REM Pre-Setup
+	REM Make sure everything is as we assume - Sanity Check time <3
 		REM Check if User edited Password =) 
 		IF "%mypassword%" == "NEVERSHAREYOURBATCHFILESORPASSWORD!" Echo [ERROR:] You didn't change the password, please make sure to edit "mypassword" of the File "Launcher.bat" in %cd% (Rightclick and choose edit) && goto Quit
 		REM Check if Node exists where we expect it 
 		IF EXIST "%NodeLocation%\mwc.exe" (
 			Echo [INFO:] Located Node
 		) ELSE (
-			Echo [ERROR:] Cannot locate Node! Please make sure your mwc.exe is actually saved under %NodeLocation% or edit the variable NodeLocation
+			Echo [ERROR:] Cannot locate Node! && Echo Please make sure your mwc.exe is actually saved under: &&	Echo %NodeLocation% or edit the variable NodeLocation
 			goto Quit
 		)
 		REM Check if CLI Wallet exists where we expect it 		
 		IF EXIST "%WalletLocation%\mwc-wallet.exe" (
 			Echo [INFO:] Located Wallet
 		) ELSE (
-			Echo [ERROR:] Cannot locate Wallet! 
-			Echo Please make sure your mwc.exe is actually saved under:
-			Echo %WalletLocation% or edit the variable WalletLocation
+			Echo [ERROR:] Cannot locate Wallet! && Echo Please make sure your mwc-wallet.exe is actually saved under: && Echo %WalletLocation% or edit the variable WalletLocation
 			goto Quit
 		)	
 		REM Check if Ngrok exists where we expect it 
@@ -64,67 +54,49 @@ REM Setup
 		REM Just "Log" our Slatefiles just in case, Code shouldn't delete stuff ;) 	
 		IF EXIST "%WalletLocation%\Backups\" (
 			If "%Debugmode%" == "TRUE" Echo [INFO:] Located Backup Folder for processed Slatefiles
-			
 		) ELSE (
 			mkdir %WalletLocation%\Backups\
 			If "%Debugmode%" == "TRUE" Echo [INFO:] Created Backup Folder for processed Slatefiles in %WalletLocation%\Backups\
 		)
-			
-		
-			
-REM Setup Node as needed for everything
-	cd %NodeLocation%\
-	start /min mwc.exe
-	Echo [WARN:] MWC-Node starting please give it a Moment to synchronize!
-	Echo.
-		
-REM Define Interactive modes (Ask for startup vars?)
-	Echo.
-	Echo "What do you want to do? (Type letter and press Enter)"
-	Echo.
+	REM Setup Node as needed for everything
+	cd %NodeLocation%\ && start /min mwc.exe
+	Echo [WARN:] MWC-Node starting please give it a Moment to synchronize! && Echo. && Echo. && Echo "What do you want to do? (Type letter and press Enter)"
+	REM Define Interactive modes (Ask for startup vars?)
+	
 	set /p mode=(S)end, (F)inalize, (L)isten, (I)nfo, (C)ommandprompt, (Q)uit 
 	Echo.
 		GOTO %mode%
-
 REM ####Modes####
-	
 :S
 :s	
-
-	REM Go in Wallet Dir
+REM Go in Wallet Dir
 	cd %WalletLocation%\
-	REM Send a transaction, ask which mode 
-	Echo (Hint: Type "File" or "HTTP" completely!)
-	Echo.
+REM Send a transaction, ask which mode 
+	Echo (Hint: Type "File" or "HTTP" completely!) && Echo.
 	set /p method=Send by (File) or (HTTP) 
 	Echo.
 	set /p Amount=What Amount to send? (Enter how many MWC you want to send as a number and press Enter)   
-	Echo.
-			
-	goto %method%
-		
-		:File
-		:file
-		
-		REM Move old Slatefile in Backupfolder
-		IF EXIST "%WalletLocation%\%TransactionFilename%" (
-			Echo [INFO:] Located a TransactionFile in Walletfolder, Moving to Backupfolder so we can create a new one
-			If "%Debugmode%" == "TRUE" Echo [INFO:]Moved old Slatefile to Backups before creating new one
-			move "%WalletLocation%\%TransactionFilename%" "%WalletLocation%\Backups\%DATE%_%TIME%__%TransactionFilename%" >NULL
-		) ELSE (
-		REM not needed but here cuz im lazy, find the egg, keep it =) 
-		)
-		
-			mwc-wallet.exe -p %mypassword% send -m file -d %TransactionFilename% %Amount%
-			Echo [INFO:]Your payment File will be located in %WalletLocation%\%TransactionFilename%
-			Echo.
-				goto Redo
-		:HTTP
-		:http
-			mwc-wallet.exe -p %mypassword% send -d %URL% %Amount%
-			Echo.
-				goto Redo
-				
+	Echo.		
+		goto %method%
+			:File
+			:file
+			REM Move old Slatefile in Backupfolder
+			IF EXIST "%WalletLocation%\%TransactionFilename%" (
+				Echo [INFO:] Located a TransactionFile in Walletfolder, Moving to Backupfolder so we can create a new one
+				If "%Debugmode%" == "TRUE" Echo [INFO:]Moved old Slatefile to Backups before creating new one
+				move "%WalletLocation%\%TransactionFilename%" "%WalletLocation%\Backups\%DATE%_%TIME%__%TransactionFilename%" > nul 2>&1
+			) ELSE (
+				REM not needed but here cuz im lazy, find the egg, keep it =) 
+			)
+				mwc-wallet.exe -p %mypassword% send -m file -d %TransactionFilename% %Amount%
+				Echo [INFO:]Your payment File will be located in %WalletLocation%\%TransactionFilename%
+				Echo.
+					goto Redo
+			:HTTP
+			:http
+				mwc-wallet.exe -p %mypassword% send -d %URL% %Amount%
+				Echo.
+					goto Redo
 :F
 :f
 	REM Go in Wallet Dir
@@ -136,60 +108,37 @@ REM ####Modes####
 	) ELSE (
 		If "%Debugmode%" == "TRUE" ECHO Searching a Responsefile in Downloads, Walletfolder was empty
 	)
-		IF EXIST "c:\users\%username%\Downloads\%Responsefilename%" move "c:\users\%username%\Downloads\%Responsefilename%" "%WalletLocation%\%Responsefilename%" >NULL
+		IF EXIST "c:\users\%username%\Downloads\%Responsefilename%" move "c:\users\%username%\Downloads\%Responsefilename%" "%WalletLocation%\%Responsefilename%" > nul 2>&1
 		REM Found it and moved it, no need to inform so bail 
 		IF EXIST "%WalletLocation%\%Responsefilename%" goto finishFinalize
 		Echo [WARN:] Cannot locate Responsefilename! (Not in Downloads nor in WalletFolder) 
 		Echo Please make sure your Responsefile is actually named  %Responsefilename% 
-		
-		
 	:finishFinalize
 	REM Finalize a transaction
-	Echo.
-	Echo.
-	Echo.
+	Echo. && Echo. && Echo.
 	mwc-wallet.exe -p %mypassword% finalize -i %Responsefilename%
 	REM Wait for Slatefile to be accessible again to move it when done (just to make sure it isnt locked)
-	timeout 5 >NULL
+	timeout 5 > nul 2>&1
 	If "%Debugmode%" == "TRUE" ECHO Moving processed Slate File into Backup Folder
-	move "%WalletLocation%\%Responsefilename%" "%Backupfolder%\%DATE%_%TIME%__%Responsefilename%" >NULL
-	Echo.
-	goto Redo
-	
+	move "%WalletLocation%\%Responsefilename%" "%Backupfolder%\%DATE%_%TIME%__%Responsefilename%" > nul 2>&1
+	Echo. 
+		goto Redo
 :L
 :l
-	REM Go in Wallet Dir
-	cd %WalletLocation%\
-	REM Wallet listen mode
-	start cmd.exe /c "mwc-wallet.exe -p %mypassword% listen"
-	Echo.
-	Echo.
-	Echo.
-	Echo.
-	Echo Wallet is listening now =) 
+	REM Go in Wallet Dir and start Wallet listen mode
+	cd %WalletLocation%\ && start /min cmd.exe /c "mwc-wallet.exe -p %mypassword% listen"
+	Echo. && Echo. && Echo. && Echo Wallet is listening now =) 
 	rem Echo Enter your password in the newly entered windows and your Wallet will be listening!
 	set /p UsingNgrok=Should we start Ngrok? (Y)es or (N)o (Enter Letter in parenthesis and press Enter)   
-	Echo.
-	IF "%UsingNgrok%" == "Y" (
-	goto ng
-	) 
-	IF "%UsingNgrok%" == "y" (
-	goto ng
-	) 
-	IF "%UsingNgrok%" == "yes" (
-	goto ng
-	)
-	IF "%UsingNgrok%" == "Yes" (
-	goto ng
-	)
-	Echo.
-	goto Redo
+	IF "%UsingNgrok%" == "Y" IF "%UsingNgrok%" == "y" IF "%UsingNgrok%" == "yes" IF "%UsingNgrok%" == "Yes"	goto ng
+	Echo. && Echo. 
+		goto Redo
 :ng
 	cd %NgrokLocation%\
 	start cmd.exe /c ngrok.exe http 3415
 	ECHO Use The HTTP Forwarding Addres displayed by Ngrok for Withdrawals (Only valid for 8 Hours!)
 	Echo.
-	goto Redo
+		goto Redo
 :I
 :i
 	REM Go in Wallet Dir
@@ -198,16 +147,13 @@ REM ####Modes####
 	Echo.
 
 :Redo
-	Echo.
-	Echo.
-	Echo.
+	Echo. && Echo. && Echo.
 	REM Define Interactive modes (Ask for startup vars?)
 	Echo "Anything else you want to do? (Type letter and press Enter)"
 	Echo.
 	set /p mode=(S)end, (F)inalize, (L)isten, (I)nfo, (C)ommandprompt, (Q)uit   
 	Echo.
 		GOTO %mode%
-		
 :Q
 :Quit
 	Echo.
@@ -221,21 +167,15 @@ REM ####Modes####
 	taskkill /IM ngrok.exe  /F
 	taskkill /IM cmd.exe  /F
 	quit
-	
 :C
 :c
-REM Interactive Shell/Commandprompt overtakes this current session, give control back to user 	
-REM Go in Wallet Dir
-cd %WalletLocation%\
-cmd /k Echo type mwc-wallet.exe help to get a list of commands!
+REM Interactive Shell/Commandprompt 	
+REM Go in Wallet Dir and give control back to user
+cd %WalletLocation%\ && cmd /k Echo type mwc-wallet.exe help to get a list of commands!
 
-
-
-
-REM Author MrT, Version 0.4
+REM Author MrT, Version 0.4.4
 :: Just a "Wrapper" around MWC Node and Wallet to make interaction a bit more Userfriendly
 :: Replace the Variables if Needed I assume the following: 
 :: This File, MWC-Wallet.exe and MWC.exe are all in the same Folder!
 :: Hint: You will need to type your password for some interactions, please stay safe out there!
 REM Never share customized scripts that might contain passwords! (Hence this Script doesnt ask for a password or gives a way to include it)
-
